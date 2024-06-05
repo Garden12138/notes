e311a9f4596b911436aa161ebe586b70e0b418d5
FSDGarden <847686279@qq.com>
2024-06-05 11:42:32 +0800

    feat:添加filex2

--- python3/file.py
+++ python3/file.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 
 print("--------only_read--------")
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_read.txt", "r") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_read.txt", "r") as f: # file must exist
     print(f.read(4))
     print(f.read())
 
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_read.txt", "r") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_read.txt", "r") as f: # file must exist
     print(f.readline(4))
     print(f.readline())
 
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_read.txt", "r") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_read.txt", "r") as f: # file must exist
     print(f.readlines(32))
     print(f.readlines())
 
 
 print("--------only_write--------")
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_write.txt", "w") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_write.txt", "w") as f: # file will be created if not exist
     f.write("Hello, world1\n")
     f.writelines(["Hello, world2\n", "Hello, world3"])
 
 print("--------only_append--------")
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_append.txt", "a") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_append.txt", "a") as f: # 
+
     f.write("Hello, world4\n")
 
 print("--------read_write--------")
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/read_write_r+.txt", "r+") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/read_write_r+.txt", "r+") as f: # file must exist
     f.write("H")
     print(f.read())
 
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/read_write_w+.txt", "w+") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/read_write_w+.txt", "w+") as f: # file will be created if not exist
     f.write("Hello, world\n")
     print(f.read()) # empty
 
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/read_write_a+.txt", "a+") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/read_write_a+.txt", "a+") as f: # file will be created if not exist
     f.write("Hello, world\n")
     print(f.read()) # empty
 
 print("--------others--------")
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/others.txt", "rb+") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/others.txt", "rb+") as f: # file must exist
     f.write(b'0123456789abcdef')
     print(f.tell())
     f.seek(1, 0)
@@ -46,10 +47,10 @@ with open("/Users/cengjiada/Documents/study/learn-note/python3/file/others.txt",
     print(f.tell(), f.read(1))
     f.seek(-8, 2)
     print(f.tell(), f.read(1))
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_write.txt", "r+") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/only_write.txt", "r+") as f: # file must exist
     f.truncate(3)
     print(f.readlines())
-with open("/Users/cengjiada/Documents/study/learn-note/python3/file/others.txt", "r+") as f:
+with open("/Users/cengjiada/Documents/study/learn-note/python3/file/others.txt", "r+") as f: # file must exist
     fid = f.fileno()
     print(fid)
     satty = f.isatty()
